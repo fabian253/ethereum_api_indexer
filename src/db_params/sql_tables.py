919bb64241c1c925cb5a9fafc735c2a6dd3d6b58
Fabian Galm <fabian.galm@gmail.com>
2023-05-15 22:57:00 +0200

    updated tables

--- src/db_params/sql_tables.py
+++ src/db_params/sql_tables.py
@@ -3,6 +3,7 @@ CONTRACT_TABLE = (
     "contract_address char(42) PRIMARY KEY UNIQUE NOT NULL,"
     "name varchar(200) DEFAULT NULL,"
     "symbol varchar(50) DEFAULT NULL,"
+    "block_deployed int DEFAULT NULL,"
     "total_supply varchar(100) DEFAULT NULL,"
     "ERC20 bool NOT NULL DEFAULT FALSE,"
     "ERC20Metadata bool NOT NULL DEFAULT FALSE,"
@@ -14,5 +15,17 @@ CONTRACT_TABLE = (
     "ERC1155 bool NOT NULL DEFAULT FALSE,"
     "ERC1155TokenReceiver bool NOT NULL DEFAULT FALSE,"
     "abi json DEFAULT NULL"
+    ")"
+)
+
+TRANSACTION_TABLE = (
+    "transaction ("
+    "transaction_hash char(66) PRIMARY KEY UNIQUE NOT NULL,"
+    "contract_address char(42) NOT NULL,"
+    "token_id int DEFAULT NULL,"
+    "value varchar(100) DEFAULT NULL,"
+    "from_address char(42) NOT NULL,"
+    "to_address char(42) NOT NULL,"
+    "block_number int NOT NULL"
     ")"
 )

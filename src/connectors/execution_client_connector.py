77a598d481c9653e61e2ab6fb8a4b6cc783cb600
Fabian Galm <fabian.galm@gmail.com>
2023-05-15 22:56:48 +0200

    rebuild connectors

--- src/connectors/execution_client_connector.py
+++ src/connectors/execution_client_connector.py
@@ -1,14 +1,23 @@
 from web3 import Web3
-from web3.exceptions import BlockNotFound
+from web3.exceptions import NoABIFound
 import json
+import requests
 from enum import Enum
 from os import listdir
+from connectors.sql_database_connector import SqlDatabaseConnector
+
 
 timeout = 60
 
+# init token standards
 TokenStandard = Enum("TokenStandard", [(token_standard.replace(
     ".json", ""), token_standard.replace(
-    ".json", "")) for token_standard in listdir(f"src/token_standard")])
+    ".json", "")) for token_standard in listdir(f"src/token_standard") if token_standard.endswith(".json")])
+
+token_standards = {}
+for token_standard in TokenStandard:
+    with open(f"src/token_standard/{token_standard.name}.json", "r") as f:
+        token_standards[token_standard.name] = json.load(f)
 
 
 class ExecutionClientConnector:
@@ -17,14 +26,21 @@ class ExecutionClientConnector:
                  execution_client_url,
                  etherscan_ip: str,
                  etherscan_api_key: str,
-                 token_standards: dict
+                 sql_db_connector: SqlDatabaseConnector,
+                 contract_table_name: str
                  ) -> None:
+        # execution client params
+        self.execution_client_url = execution_client_url
+        self.token_standards = token_standards
+        # set etherscan api params
         self.etherscan_ip = etherscan_ip
         self.etherscan_api_key = etherscan_api_key
-        self.token_standards = token_standards
+        # set sql db connector
+        self.sql_db_connector = sql_db_connector
+        self.contract_table_name = contract_table_name
         # init execution client
         self.execution_client = Web3(Web3.HTTPProvider(
-            execution_client_url, request_kwargs={'timeout': timeout}))
+            self.execution_client_url, request_kwargs={'timeout': timeout}))
 
     def get_block_number(self):
         response = self.execution_client.eth.get_block_number()
@@ -32,17 +48,85 @@ class ExecutionClientConnector:
         return {"block_number": response}
 
     def get_block(self, block_identifier=None, full_transactions=False):
-        try:
-            if block_identifier is None:
-                response = self.execution_client.eth.get_block(
-                    self.execution_client.eth.default_block, full_transactions)
-            else:
-                response = self.execution_client.eth.get_block(
-                    block_identifier, full_transactions)
-        except (BlockNotFound, ValueError):
-            return None
+        if block_identifier is None:
+            response = self.execution_client.eth.get_block(
+                self.execution_client.eth.default_block, full_transactions)
+        else:
+            response = self.execution_client.eth.get_block(
+                block_identifier, full_transactions)
 
-        return json.loads(Web3.toJSON(response))
+        return json.loads(Web3.to_json(response))
+
+    def get_transaction(self, transaction_hash: str, decode_input: bool = True):
+        response = self.execution_client.eth.get_transaction(
+            transaction_hash)
+
+        response = json.loads(Web3.to_json(response))
+
+        if decode_input and response["input"] != "0x":
+            contract_address = response["to"]
+
+            contract_abi = self.get_contract_abi(contract_address)
+
+            contract = self.execution_client.eth.contract(
+                address=contract_address, abi=contract_abi)
+
+            func_obj, func_params = contract.decode_function_input(
+                response["input"])
+
+            for param_name, param in func_params.items():
+                if type(param) is int:
+                    func_params[param_name] = str(param)
+                if type(param) is bytes:
+                    func_params[param_name] = f"0x{param.hex()}"
+
+            decoded_input = {
+                "function": func_obj,
+                "params": func_params
+            }
+
+            response["input_decoded"] = decoded_input
+
+        return response
+
+    def get_token_standard_abi(self, token_standard: TokenStandard):
+        return self.token_standards[token_standard.name]
+
+    def get_contract_abi(self, contract_address: str):
+        """
+        Return contract abi.
+        Query from db if in db else query from etherscan (no other way)
+        """
+        if self.sql_db_connector.is_contract_in_db(self.contract_table_name, contract_address):
+            contract_data = self.sql_db_connector.query_all_contract_data(
+                self.contract_table_name, contract_address)
+            return contract_data["abi"]
+        else:
+            # contract abi can not be retrieved from blockchain (not with get_code()) -> etherscan is needed
+            params = {
+                "module": "contract",
+                "action": "getabi",
+                "address":  contract_address,
+                "apikey": self.etherscan_api_key
+            }
+            response = requests.get(self.etherscan_ip, params=params)
+
+            if response.json()["status"] == "0":
+                raise NoABIFound
+
+            contract_abi = json.loads(response.json()["result"])
+
+            contract_metadata = self.get_contract_metadata(
+                contract_address, contract_abi)
+
+            contract_implemented_token_standards = self.get_contract_implemented_token_standards(
+                contract_address, contract_abi)
+
+            # insert data into db
+            self.sql_db_connector.insert_contract_data(
+                self.contract_table_name, contract_address, contract_metadata, contract_implemented_token_standards, contract_abi)
+
+            return contract_abi
 
     def get_contract_implemented_token_standards(self, contract_address: str, contract_abi=None):
         # TODO: improve filter function with more than function name
@@ -75,7 +159,7 @@ class ExecutionClientConnector:
             contract_abi = self.get_contract_abi(contract_address)
 
         contract = self.execution_client.eth.contract(
-            Web3.toChecksumAddress(contract_address), abi=contract_abi)
+            Web3.to_checksum_address(contract_address), abi=contract_abi)
 
         contract_function = contract.functions[function_name]
 
@@ -83,7 +167,7 @@ class ExecutionClientConnector:
 
         return response
 
-    def get_contract_mint_block(self, contract_address: str):
+    def get_contract_deploy_block(self, contract_address: str):
         try:
             event_filter = self.execution_client.eth.filter({
                 "fromBlock": 0,
@@ -118,6 +202,10 @@ class ExecutionClientConnector:
         except:
             token_symbol = None
         try:
+            deploy_block = self.get_contract_deploy_block(contract_address)
+        except:
+            deploy_block = None
+        try:
             token_total_supply = self.execute_contract_function(
                 contract_address, "totalSupply", contract_abi)
         except:
@@ -127,5 +215,6 @@ class ExecutionClientConnector:
             "address": contract_address,
             "name": token_name,
             "symbol": token_symbol,
+            "block_deployed": deploy_block,
             "total_supply": token_total_supply
         }
--- src/connectors/sql_database_connector.py
+++ src/connectors/sql_database_connector.py
@@ -1,79 +1,168 @@
 import mysql.connector
 from mysql.connector import errorcode
+from mysql.connector.errors import DatabaseError
 import json
 from typing import Union
+import logging
 
 
 class SqlDatabaseConnector:
 
-    def __init__(self, host: str, port: int, user: str, password: str) -> None:
-        config = {
+    def __init__(self, host: str, port: int, user: str, password: str, db_name: str, tables: list = None, with_logging: bool = True) -> None:
+        # set logging
+        if with_logging:
+            logging.basicConfig(level=logging.INFO)
+        else:
+            logging.basicConfig(level=logging.ERROR)
+
+        self.config = {
             'user': user,
             'password': password,
             'host': host,
             'port': port
         }
+        self.db_name = db_name
 
-        self.connection = mysql.connector.connect(**config)
+        self.connection = None
+        try:
+            self.connect(self.config)
+
+            self.db_name = db_name
+            self.use_database(db_name)
+
+            for table in tables:
+                self.create_table(table)
+        except DatabaseError:
+            pass
 
     def __del__(self):
-        self.connection.close()
+        if self.connection is not None:
+            self.connection.close()
+
+    # General Functions
+
+    def connect(self, config: Union[dict, None] = None):
+        try:
+            if config is not None:
+                self.connection = mysql.connector.connect(**config)
+                logging.info("SQL server connected")
+            elif self.connection is None or not self.connection.is_connected():
+                self.connection = mysql.connector.connect(**self.config)
+                logging.info("SQL server connected")
+        except DatabaseError:
+            raise DatabaseError("Database not available")
 
     def create_database(self, db_name: str):
+        self.connect()
+
         cursor = self.connection.cursor()
         try:
             cursor.execute(
                 f"CREATE DATABASE {db_name} DEFAULT CHARACTER SET 'utf8'")
+            logging.info(f"Created database: {db_name}")
         except mysql.connector.Error as err:
-            print(f"Failed creating database: {err}")
+            logging.error(f"Failed creating database: {db_name}, error: {err}")
 
         cursor.close()
 
     def use_database(self, db_name: str):
-        cursor = self.connection.cursor()
-        try:
-            cursor.execute(f"USE {db_name}")
-        except mysql.connector.Error as err:
-            print(f"Database {db_name} does not exists.")
-            if err.errno == errorcode.ER_BAD_DB_ERROR:
-                self.create_database(db_name)
-                print(f"Database {db_name} created successfully.")
-                self.connection.database = db_name
-            else:
-                print(err)
+        self.connect()
 
+        db_in_use = self.get_database_in_use()
+
+        if db_in_use is None or db_in_use != db_name:
+            cursor = self.connection.cursor()
+            try:
+                cursor.execute(f"USE {db_name}")
+                logging.info(f"Using database: {db_name}")
+            except mysql.connector.Error as err:
+                logging.error(f"Database does not exist: {db_name}")
+                if err.errno == errorcode.ER_BAD_DB_ERROR:
+                    self.create_database(db_name)
+                    self.connection.db_name = db_name
+                else:
+                    print(err)
+
+            cursor.close()
+
+    def get_database_in_use(self) -> str:
+        self.connect()
+
+        cursor = self.connection.cursor()
+        cursor.execute("select database()")
+        database = cursor.fetchone()[0]
         cursor.close()
 
+        return database
+
     def create_table(self, table_description: str):
+        self.use_database(self.db_name)
+
+        table_name = table_description.split(" ", 1)[0]
+
         cursor = self.connection.cursor()
         try:
-            print("Creating table: ", end='')
             cursor.execute(f"CREATE TABLE {table_description}")
+            logging.info(f"Created table: {table_name}")
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                print("already exists.")
+                logging.info(f"Created table already exists: {table_name}")
             else:
-                print(err.msg)
-        else:
-            print("OK")
+                logging.error(
+                    f"Failed creating table: {table_name}, error: {err.msg}")
 
         cursor.close()
 
     def insert_data(self, table_name: str, data: dict):
-        cursor = self.connection.cursor()
+        self.use_database(self.db_name)
+
         data_fields = ", ".join(data.keys())
         data_value_slots = ", ".join([f"%({key})s" for key in data.keys()])
 
         insert_query = f"INSERT INTO {table_name} ({data_fields}) VALUES ({data_value_slots})"
 
+        cursor = self.connection.cursor()
         cursor.execute(insert_query, data)
-
         self.connection.commit()
+        cursor.close()
+
+    def insert_many_data(self, table_name: str, many_data: list[dict], batch_size: int = 10000):
+        self.use_database(self.db_name)
+
+        field_names = many_data[0].keys()
+        data_fields = ", ".join(field_names)
+        data_value_slots = ", ".join([f"%({key})s" for key in field_names])
+
+        insert_query = f"INSERT IGNORE INTO {table_name} ({data_fields}) VALUES ({data_value_slots})"
+
+        cursor = self.connection.cursor()
+
+        if len(many_data) > batch_size:
+            batch_start = 0
+            batch_end = batch_size
+
+            while batch_end <= len(many_data)+1:
+                batch = many_data[batch_start:batch_end]
+
+                cursor.executemany(insert_query, batch)
+                self.connection.commit()
+
+                # calc new batch
+                batch_start = batch_end
+                if batch_start >= len(many_data):
+                    break
+                elif batch_start + batch_start <= len(many_data):
+                    batch_end = batch_start + batch_size
+                else:
+                    batch_end = len(many_data)+1
+        else:
+            cursor.executemany(insert_query, many_data)
+            self.connection.commit()
 
         cursor.close()
 
     def query_data(self, table_name: str, fields: Union[list, str] = "*", equal_filter: dict = None, limit: int = 1000) -> list:
-        cursor = self.connection.cursor(dictionary=True, prepared=True)
+        self.use_database(self.db_name)
 
         if fields == "*":
             data_fields = fields
@@ -87,36 +176,35 @@ class SqlDatabaseConnector:
                 [f"{key} = %({key})s" for key in equal_filter.keys()])
             select_query = f"SELECT {data_fields} FROM {table_name} WHERE {data_filter_slots} LIMIT {limit}"
 
+        cursor = self.connection.cursor(dictionary=True, prepared=True)
         cursor.execute(select_query, equal_filter)
-
         data_list = cursor.fetchall()
-
         cursor.close()
 
         return data_list
 
     def query_data_type(self, table_name: str):
-        cursor = self.connection.cursor(dictionary=True, prepared=True)
+        self.use_database(self.db_name)
 
         select_query = f"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '{table_name}'"
 
+        cursor = self.connection.cursor(dictionary=True, prepared=True)
         cursor.execute(select_query)
-
         data_type_list = cursor.fetchall()
-
         data_type_dict = {value["COLUMN_NAME"]: value["DATA_TYPE"]
                           for value in data_type_list}
-
         cursor.close()
 
         return data_type_dict
 
-    def insert_contract_data(self, table_name: str, contract_address: str, contract_metadata: dict, block_minted: Union[int, None],  contract_implemented_token_standards: dict, contract_abi):
+    # Contract Functions
+
+    def insert_contract_data(self, table_name: str, contract_address: str, contract_metadata: dict, contract_implemented_token_standards: dict, contract_abi):
         data = {
             "contract_address": contract_address,
             "name": contract_metadata["name"],
             "symbol": contract_metadata["symbol"],
-            "block_minted": block_minted,
+            "block_deployed": contract_metadata["block_deployed"],
             "total_supply": contract_metadata["total_supply"],
         }
 
@@ -126,7 +214,7 @@ class SqlDatabaseConnector:
 
         self.insert_data(table_name, data)
 
-    def query_contract_data(self, table_name: str, contract_address: str) -> dict:
+    def query_all_contract_data(self, table_name: str, contract_address: str) -> dict:
         data = self.query_data(table_name, equal_filter={
                                "contract_address": contract_address})[0]
 
@@ -134,28 +222,56 @@ class SqlDatabaseConnector:
 
         # convert tinyint to boolean and decode json
         for column, data_type in data_types.items():
-            if data_type == "tinyint":
-                data[column] = True if data[column] == 1 else False
+            if data[column] is not None:
+                if data_type == "tinyint":
+                    data[column] = True if data[column] == 1 else False
 
-            if data_type == "json":
-                data[column] = json.loads(data[column].decode("utf-8"))
+                if data_type == "json":
+                    data[column] = json.loads(data[column].decode("utf-8"))
 
-            if data_type == "varchar" and data[column].isdigit():
-                data[column] = int(data[column])
+                if data_type == "varchar" and data[column].isdigit():
+                    data[column] = int(data[column])
 
         return data
 
-    def query_erc721_contract_addresses(self, table_name: str, limit: int = 1000) -> list:
-        return self.__query_contract_addresses(table_name, "ERC721", limit)
+    def query_contract_data(self,
+                            table_name: str,
+                            token_standard: Union[str, None] = None,
+                            with_name: bool = False,
+                            with_symbol: bool = False,
+                            with_block_deployed: bool = False,
+                            with_total_supply: bool = False,
+                            with_abi: bool = False,
+                            limit: int = 1000) -> list:
+        fields = ["contract_address"]
+        if with_name:
+            fields.append("name")
+        if with_symbol:
+            fields.append("symbol")
+        if with_block_deployed:
+            fields.append("block_deployed")
+        if with_total_supply:
+            fields.append("total_supply")
+        if with_abi:
+            fields.append("abi")
 
-    def query_erc20_contract_addresses(self, table_name: str, limit: int = 1000) -> list:
-        return self.__query_contract_addresses(table_name, "ERC20", limit)
+        if token_standard is None:
+            data = self.query_data(
+                table_name, fields, None, limit)
+        else:
+            data = self.query_data(
+                table_name, fields, {token_standard: True}, limit)
 
-    def __query_contract_addresses(self, table_name: str, token_standard: str, limit: int = 1000) -> list:
-        data = self.query_data(
-            table_name, ["contract_address"], {token_standard: True}, limit)
+        data_types = self.query_data_type(table_name)
 
-        data = [contract["contract_address"] for contract in data]
+        for d in data:
+            for key, value in d.items():
+                if value is not None:
+                    if data_types[key] == "json":
+                        d[key] = json.loads(value.decode("utf-8"))
+
+                    if data_types[key] == "varchar" and value.isdigit():
+                        d[key] = int(value)
 
         return data
 
@@ -167,3 +283,65 @@ class SqlDatabaseConnector:
             return False
         else:
             return True
+
+    # Transaction Functions
+
+    def insert_transaction_data(self, table_name: str, transaction_hash: str, contract_address: str, token_id: int, from_address: str, to_address: str, block_number: int):
+        if not self.is_transaction_in_db(table_name, transaction_hash):
+            data = {
+                "transaction_hash": transaction_hash,
+                "contract_address": contract_address,
+                "token_id": token_id,
+                "from_address": from_address,
+                "to_address": to_address,
+                "block_number": block_number
+            }
+
+            self.insert_data(table_name, data)
+
+    def insert_many_transaction_data(self, table_name: str, transaction_data: list[dict]):
+        self.insert_many_data(table_name, transaction_data)
+
+    def query_contract_transaction_data(self, table_name: str, contract_address: str, token_id: Union[int, None] = None, fields: Union[list, str] = "*"):
+        filters = {
+            "contract_address": contract_address
+        }
+        if token_id is not None:
+            filters["token_id"] = token_id
+
+        contract_transactions = self.query_data(
+            table_name, fields, filters, limit=1000000000)
+
+        #response = sorted(response, key=lambda x: x["token_id"])
+
+        return contract_transactions
+
+    def query_token_transaction_data(self, table_name: str, contract_address: str, token_id: int):
+        # return list of transactions sorted by block_number
+        fields = ["from_address", "to_address", "block_number"]
+
+        token_transactions = self.query_contract_transaction_data(
+            table_name, contract_address, token_id, fields)
+        token_transactions = sorted(
+            token_transactions, key=lambda x: x["block_number"])
+
+        return token_transactions
+
+    def is_transaction_in_db(self, table_name: str, transaction_hash: str) -> bool:
+        contract = self.query_data(table_name, equal_filter={
+                                   "transaction_hash": transaction_hash})
+
+        if len(contract) == 0:
+            return False
+        else:
+            return True
+
+    def update_contract_deploy_block(self, table_name: str, contract_address: str, block_deployed: int):
+        self.use_database(self.db_name)
+
+        insert_query = f"UPDATE {table_name} SET block_deployed={block_deployed} WHERE contract_address='{contract_address}'"
+
+        cursor = self.connection.cursor()
+        cursor.execute(insert_query)
+        self.connection.commit()
+        cursor.close()

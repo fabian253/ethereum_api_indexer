663585b91d01f785d2499f4fa185bae028f095b2
Fabian Galm <fabian.galm@gmail.com>
2023-07-22 17:32:00 +0200

    final

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ethereum_api_indexer
+# Ethereum API Indexer
+
+The Ethereum API Indexer aims to index the data of the Ethereum Blockchain. Here, the indexer refers exclusively to the master data of smart contracts.
---
+++ src/config.py
@@ -0,0 +1,25 @@
+# execution client
+EXECUTION_CLIENT_IP = "134.155.111.231"  # for local testing
+# EXECUTION_CLIENT_IP = "172.17.0.3"  # for server deployment
+EXECUTION_CLIENT_PORT = 8545
+
+# etherscan
+ETHERSCAN_URL = "https://api.etherscan.io/api"
+ETHERSCAN_API_KEY = "NZW3WAVANMS9WHFJZSB69MQFJEZ1D1E54R"
+
+# infura
+INFURA_URL = "https://mainnet.infura.io/v3"
+INFURA_API_KEY = "c2762ad3b91949a099c826439f9dc5c6"
+
+# mysql database
+SQL_DATABASE_HOST = "134.155.111.231"
+SQL_DATABASE_PORT = 3307
+SQL_DATABASE_USER = "root"
+SQL_DATABASE_PASSWORD = "admin"
+SQL_DATABASE_NAME = "ethereum_api"
+SQL_DATABASE_TABLE_CONTRACT = "contract"
+SQL_DATABASE_TABLE_TRANSACTION = "transaction"
+
+# indexing params
+INDEXING_START_BLOCK = 16000000
+INDEXING_END_BLOCK = 17000000
